e05fb72e4520fdb2c45fa3f32a1a42bb2fc2bb5d
BharadwajRachakonda <rbharadwaj022@gmail.com>
2024-10-05 13:29:10 +0530

    Fix build URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: "https://bharadwajrachakonda.github.io/todo-frontend",
+  base: "https://bharadwajrachakonda.github.io/todo-frontend",
 });
