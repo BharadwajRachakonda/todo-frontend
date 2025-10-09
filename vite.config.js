583f55dd6800eb2f9a7f81947123c45e08fca66e
Bharadwaj <rbharadwaj022@gmail.com>
2025-10-09 19:48:28 +0530

    changing domain name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://www.bharadwajrachakonda.me/todo-frontend/",
+  base: "https://www.rachakonda.tech/todo-frontend/",
 });
